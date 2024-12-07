6ad27b947f55796ff82168894fa7eb17a5b54da6
arsalannury <arsiiiland@yahoo.com>
2024-12-07 17:49:31 +0330

    --test-@max-* | @min-*

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="@container p-5">
-        <p className="text-center hello-world">Hello World !</p>
+        <p className="text-center @max-md:text-red-100">Hello World !</p>
       </div>
     </>
   );
