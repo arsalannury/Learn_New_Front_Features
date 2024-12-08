0e4598cbf399ca5f1257ea27311b2ff97aa117c8
arsalannury <arsiiiland@yahoo.com>
2024-12-08 11:26:54 +0330

    --test-transform3d

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 function App() {
   return (
     <>
-      <div className="@container p-5">
-        <p className="text-center @max-md:text-red-100">Hello World !</p>
+      {/* <div className="perspective-distant mt-28">
+        <img
+          className="m-auto text-center transform-3d -scale-y-[20%] "
+          src="/3d.png"
+        />
+      </div> */}
+      <div className="perspective-[1000px] m-26">
+        <div className="transform-3d -rotate-x-45 rotate-y-45 scale-x-200 bg-red-500 w-40 h-40">
+          Hello 3D
+        </div>
       </div>
     </>
   );
