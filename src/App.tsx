056fe23b08122f25423ec8133f934091459f6cdf
arsalannury <arsiiiland@yahoo.com>
2024-12-08 14:24:45 +0330

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ function App() {
   return (
     <>
       <button
-        className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all cursor-pointer duration-[2000ms]
+        className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all cursor-pointer duration-300
          hover:bg-blue-500 hover:shadow-lg
-       bg-blue-400 rounded-md text-white font-bold shadow-sm inset-shadow-sm inset-shadow-white/15 inset-ring inset-ring-white/15 starting:opacity-0"
+       bg-blue-400 rounded-md text-white font-bold shadow-sm inset-shadow-sm inset-shadow-white/15 inset-ring inset-ring-white/15 "
       >
         Click The Button !
       </button>
