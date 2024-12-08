a87ae34e1cff9c0e21a0ee5a4688865a3b16cc44
arsalannury <arsiiiland@yahoo.com>
2024-12-08 12:23:10 +0330

    --test-bg-radial-bg-conic

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ function App() {
   return (
     <>
       <div
-        className="transition-all duration-1000 hover:rotate-180
-                      rounded-xl w-40 h-40
-                      bg-linear-to-l from-blue-300 via-blue-400  to-blue-600 hover:shadow-sm
-                      absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+        className="rounded-full w-40 h-40
+                   bg-conic from-blue-300 via-blue-400  to-blue-600
+                   absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
       />
     </>
   );
