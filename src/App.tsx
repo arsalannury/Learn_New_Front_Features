347d9c7b758f8fca234bf15e7ef5ec810a35538c
arsalannury <arsiiiland@yahoo.com>
2024-12-08 12:52:29 +0330

    --test-inset-shadow-ring

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 function App() {
   return (
     <>
-      <div
-        className="rounded-full w-40 h-40
-                   bg-conic from-blue-300 via-blue-400  to-blue-600
-                   absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
-      />
+      <button
+        className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all cursor-pointer
+         hover:bg-blue-500 hover:shadow-lg
+       bg-blue-400 rounded-md text-white font-bold shadow-sm inset-shadow-sm inset-shadow-white/15 inset-ring inset-ring-white/15 "
+        // shadow-md inset-shadow-sm inset-shadow-white/20 ring ring-blue-600 inset-ring inset-ring-white/15
+      >
+        Click The Button !
+      </button>
     </>
   );
 }
