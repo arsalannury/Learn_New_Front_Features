221c771d886753e22a148d644222a61361c9498c
arsalannury <arsiiiland@yahoo.com>
2024-12-08 11:50:36 +0330

    --test-bg-linear

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 function App() {
   return (
     <>
-      {/* <div className="perspective-distant mt-28">
-        <img
-          className="m-auto text-center transform-3d -scale-y-[20%] "
-          src="/3d.png"
-        />
-      </div> */}
-      <div className="perspective-[1000px] m-26">
-        <div className="transform-3d -rotate-x-45 rotate-y-45 scale-x-200 bg-red-500 w-40 h-40">
-          Hello 3D
-        </div>
-      </div>
+      <div
+        className="transition-all duration-1000 hover:rotate-180
+                      rounded-xl w-40 h-40
+                      bg-linear-to-l from-blue-300 via-blue-400  to-blue-600 hover:shadow-sm
+                      absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+      />
     </>
   );
 }
