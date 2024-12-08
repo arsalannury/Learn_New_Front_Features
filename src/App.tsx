8c07d6d6cdc0017f5a9027323e9a0dffa862cbcf
arsalannury <arsiiiland@yahoo.com>
2024-12-08 13:06:40 +0330

    --test-field-siaing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 function App() {
   return (
     <>
-      <button
-        className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all cursor-pointer
-         hover:bg-blue-500 hover:shadow-lg
-       bg-blue-400 rounded-md text-white font-bold shadow-sm inset-shadow-sm inset-shadow-white/15 inset-ring inset-ring-white/15 "
-        // shadow-md inset-shadow-sm inset-shadow-white/20 ring ring-blue-600 inset-ring inset-ring-white/15
-      >
-        Click The Button !
-      </button>
+      <textarea
+        className="resize-none absolute top-[50%] left-[50%]
+       translate-x-[-50%] translate-y-[-50%]
+       field-sizing-content outline-0 w-[200px] border-[1px]
+      border-slate-200 focus:border focus:border-slate-300 rounded p-3"
+      />
     </>
   );
 }
