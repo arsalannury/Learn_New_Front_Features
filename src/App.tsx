130bb829578d96f3c3e80bc4c4523908d9d11034
arsalannury <arsiiiland@yahoo.com>
2024-12-09 14:40:21 +0330

    --test-inert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useRef } from "react";
+
 function App() {
+  const inertRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (inertRef.current) {
+      inertRef.current?.setAttribute("inert", "");
+    }
+  }, []);
+
   return (
     <>
       <button
-        disabled
+        ref={inertRef}
         className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
                    transition-all cursor-pointer duration-300 hover:bg-blue-500 hover:shadow-lg
-                   bg-blue-400 rounded-md text-white font-bold not-disabled:bg-red-200"
+                   bg-blue-400 rounded-md text-white font-bold inert:opacity-40"
       >
         Click The Button !
       </button>
