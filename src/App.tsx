b82ca48b8d318ad9b4b889aecad8072758e880dc
arsalannury <arsiiiland@yahoo.com>
2024-12-09 14:30:39 +0330

    --test-supports-*-not-supports-*

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ function App() {
   return (
     <>
       <button
-        className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all cursor-pointer duration-300
-         hover:bg-blue-500 hover:shadow-lg
-       bg-blue-400 rounded-md text-white font-bold shadow-sm inset-shadow-sm inset-shadow-white/15 inset-ring inset-ring-white/15 "
+        disabled
+        className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
+                   transition-all cursor-pointer duration-300 hover:bg-blue-500 hover:shadow-lg
+                   bg-blue-400 rounded-md text-white font-bold not-disabled:bg-red-200"
       >
         Click The Button !
       </button>
