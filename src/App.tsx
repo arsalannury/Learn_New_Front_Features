03e656252e57b9bc23a82d212eff3a8f066f8915
arsalannury <arsiiiland@yahoo.com>
2024-12-09 16:20:02 +0330

    --test-nth-notend-yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { useEffect, useRef } from "react";
-
 function App() {
-  const inertRef = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    if (inertRef.current) {
-      inertRef.current?.setAttribute("inert", "");
-    }
-  }, []);
-
   return (
     <>
-      <button
-        ref={inertRef}
-        className="p-3 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
-                   transition-all cursor-pointer duration-300 hover:bg-blue-500 hover:shadow-lg
-                   bg-blue-400 rounded-md text-white font-bold inert:opacity-40"
-      >
-        Click The Button !
-      </button>
+      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] nth-[odd]:text-orange-300">
+        <p className="mt-2 font-bold">First Child</p>
+        <p className="mt-2 font-bold">Second Child</p>
+        <p className="mt-2 font-bold">Third Child</p>
+        <p className="mt-2 font-bold">Four Child</p>
+      </div>
     </>
   );
 }
