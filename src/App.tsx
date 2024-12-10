e8929d7c745b04ea21e14fb3e76709092f660945
arsalannury <arsiiiland@yahoo.com>
2024-12-10 10:51:11 +0330

    --test-nth-didnt-work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] nth-[odd]:text-orange-300">
+      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] [&>*:nth-child(1)]:bg-red-500">
         <p className="mt-2 font-bold">First Child</p>
         <p className="mt-2 font-bold">Second Child</p>
         <p className="mt-2 font-bold">Third Child</p>
