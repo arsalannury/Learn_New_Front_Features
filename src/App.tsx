3c4f99df49ad3359d7c5ed49e97c489da5faf8c6
arsalannury <arsiiiland@yahoo.com>
2024-12-10 11:12:16 +0330

    --test-group

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
     <>
-      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] [&>*:nth-child(1)]:bg-red-500">
-        <p className="mt-2 font-bold">First Child</p>
+      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group">
+        <p className="mt-2 font-bold group-hover:bg-red-500 transition-all duration-300">
+          First Child
+        </p>
         <p className="mt-2 font-bold">Second Child</p>
         <p className="mt-2 font-bold">Third Child</p>
         <p className="mt-2 font-bold">Four Child</p>
