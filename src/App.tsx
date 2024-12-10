68efebfb8af4ec9750337dc0b5d1b1b52ea1d76a
arsalannury <arsiiiland@yahoo.com>
2024-12-10 11:40:19 +0330

    --test-**

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 function App() {
   return (
     <>
-      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group">
-        <p className="mt-2 font-bold group-hover:bg-red-500 transition-all duration-300">
-          First Child
-        </p>
-        <p className="mt-2 font-bold">Second Child</p>
-        <p className="mt-2 font-bold">Third Child</p>
-        <p className="mt-2 font-bold">Four Child</p>
+      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] **:data-id-id:w-[100px]">
+        <img src="/3d.png" data-id-id="n" />
       </div>
     </>
   );
