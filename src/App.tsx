33d2f0714b4b60eac0bd4bfe355390455d93e277
arsalannury <arsiiiland@yahoo.com>
2024-12-10 16:49:58 +0330

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <>
-      <p className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-xxl font-bold italic daghol">
-        Hello World !
-      </p>
+     
     </>
   );
 }
