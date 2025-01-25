697610f4f1ffdd8bf2e22ba84d53577074d4c742
arsalannury <arsiiiland@yahoo.com>
2025-01-25 14:40:45 +0330

    [Transition] - [React19]

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import { useState, useTransition } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [filteredData, setFilteredData] = useState<string[]>([]);
   const [isPending, startTransition] = useTransition();
-  const [val, setVal] = useState("");
+
+  const data = Array.from({ length: 10000 }, (_, index) => `Item ${index + 1}`);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+    console.log(event.target.value);
+
+    startTransition(() => {
+      const filtered = data.filter((item) => item.includes(value));
+      setFilteredData(filtered);
+    });
+  };
 
   return (
-    <>
-      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center flex-col justify-around">
-        <p>{isPending ? "Pending..." : val}</p>
-        <input
-          style={{ border: "1px solid #000" }}
-          type="text"
-          name="name"
-          id="name"
-          onChange={(e) => {
-            startTransition(() => {
-              setVal(e.target.value);
-            });
-          }}
-        />
-      </div>
-    </>
+    <div className="flex items-center justify-center flex-col p-3">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Filter items"
+        className="border-white border-1 outline-0"
+      />
+      {isPending && <p className="font-bold mt-10">Loading...</p>}
+      <ul className="list-none mt-20 flex items-center justify-evenly flex-wrap">
+        {filteredData.map((item, index) => (
+          <li className="text-blue-400" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
