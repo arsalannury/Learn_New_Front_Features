18bb97b7ad72f0becfed6671f84890222ef09832
arsalannury <arsiiiland@yahoo.com>
2025-01-26 16:45:59 +0330

    [React19] - [useOptimistic]

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
-import { useOnline } from "./hooks/useOnline";
-import { useWindowWidth } from "./hooks/useWindowWidth";
+import { useEffect, useOptimistic, useState, useTransition } from "react";
+
+interface IItem {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+}
 
 function App() {
-  const isOnline = useOnline();
-  const windowWidth = useWindowWidth();
+  const [todos, setTodos] = useState([]);
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const getUsers = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const response = await getUsers.json();
+        setTodos(response);
+      } catch (error) {
+        setTodos([]);
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
+  const [optimisticUsersList, setOptimisticUsersList] = useOptimistic(todos);
+
+  const handleClick = (item: IItem) => {
+    startTransition(() => {
+      setOptimisticUsersList((prevTasks) =>
+        prevTasks.filter((task: IItem) => task.id !== item.id)
+      );
+    });
+  };
 
   return (
     <>
-      <p>{isOnline ? "Online" : "Offline"}</p>
-      <p>{windowWidth}</p>
+      <ul className="p-5">
+        {optimisticUsersList &&
+          optimisticUsersList.length > 0 &&
+          optimisticUsersList.map((item: IItem) => (
+            <li
+              key={item.id}
+              className="mt-5 cursor-pointer"
+              onClick={() => handleClick(item)}
+            >
+              {item.title}
+            </li>
+          ))}
+      </ul>
     </>
   );
 }
